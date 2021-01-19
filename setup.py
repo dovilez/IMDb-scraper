d4de72d6a46d7992d1e54bc9904ce0751c83cec2
Žaltauskaitė Dovilė <dovzal1@ktu.lt>
2021-01-19 16:37:31 +0200

    add files

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="imdb-scraper",
+    version="0.0.1",
+    author="dzalta",
+    author_email="dovilezal@gmail.com",
+    description="IMDb scraper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dovilez/IMDb-scraper",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pandas', 'beautifulsoup4'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
